04176739b30e0d969ed71dc784d9518dac6964e9
Yeonho Jang <siabard@gmail.com>
2024-04-15 21:23:37 +0900

    DiaryDetail - 화면 폭 및 배치 수정

--- components/diary/DiaryDetail.js
+++ components/diary/DiaryDetail.js
@@ -1,50 +1,50 @@
 import { StyleSheet, Text, View } from "react-native";
-import { WebView } from 'react-native-webview';
+import { WebView } from "react-native-webview";
 
-import * as Font from '../../constants/Font';
-import * as Colors from '../../constants/Colors';
+import * as Font from "../../constants/Font";
+import * as Colors from "../../constants/Colors";
 import Button from "../account/Button";
 import { toastMessage } from "../../utils/Utilities";
 
 const DiaryDetail = ({ methods, screenStates }) => {
-    const { user, diary } = screenStates;
-    const { content } = diary;
+  const { user, diary } = screenStates;
+  const { content } = diary;
 
-    const Container = View;
+  const Container = View;
 
-    const deleteDiary = () => {
-        methods.deleteDiary(diary.id, (rowsAffected) => {
-            toastMessage(`Successfully deleted ${rowsAffected}`);
-            methods.goBack();
-        });
-    }
-    return (
-        <Container style={styles.container}>
-            <View style={styles.buttonContainer}>
-                <Button
-                    title={`Edit`}
-                    icon={`pen`}
-                    style={{ ...styles.button, backgroundColor: Colors.SUCCESS }}
-                    onPress={() => {
-                        methods.editDiary(true);
-                    }} />
-                <Button
-                    title={`Delete`}
-                    icon={`trash`}
-                    style={{ ...styles.button, backgroundColor: Colors.DANGER }}
-                    onPress={() => {
-                        deleteDiary();
-                    }}
-
-                />
-            </View>
-            <View style={styles.diaryContainer}>
-                <Text style={styles.label}>Date: {diary.date}</Text>
-                <Text style={styles.label}>Dear Diary,</Text>
-                <View style={{ height: 240, marginBottom: 10, }}>
-                    <WebView
-                        source={{
-                            html: `
+  const deleteDiary = () => {
+    methods.deleteDiary(diary.id, (rowsAffected) => {
+      toastMessage(`Successfully deleted ${rowsAffected}`);
+      methods.goBack();
+    });
+  };
+  return (
+    <Container style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button
+          title={`Edit`}
+          icon={`pen`}
+          style={{ ...styles.button, backgroundColor: Colors.SUCCESS }}
+          onPress={() => {
+            methods.editDiary(true);
+          }}
+        />
+        <Button
+          title={`Delete`}
+          icon={`trash`}
+          style={{ ...styles.button, backgroundColor: Colors.DANGER }}
+          onPress={() => {
+            deleteDiary();
+          }}
+        />
+      </View>
+      <View style={styles.diaryContainer}>
+        <Text style={styles.label}>Date: {diary.date}</Text>
+        <Text style={styles.label}>Dear Diary,</Text>
+        <View style={{ height: 240, marginBottom: 10 }}>
+          <WebView
+            source={{
+              html: `
                         <html>
                         <head>
                             <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -60,53 +60,49 @@ const DiaryDetail = ({ methods, screenStates }) => {
                             <p>${content}</p>
                         </body>
                         </html>
-                    `}}
-                    />
-                </View>
-                <Text>Love, {`\n${user.firstName}`}</Text>
-            </View>
-        </Container>
-    );
-
+                    `,
+            }}
+          />
+        </View>
+        <Text>Love, {`\n${user.firstName}`}</Text>
+      </View>
+    </Container>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        backgroundColor: Colors.WHITE,
-        paddingHorizontal: 20,
-        paddingVertical: 30,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    buttonContainer: {
-        flexDirection: 'row-reverse',
-        columnGap: 10,
-    },
-    diaryContainer: {
-        flex: 1,
-        backgroundColor: Colors.WHITE,
-        padding: 15,
-        marginTop: 20,
-        borderRadius: 5,
-        borderWidth: 0.25,
-
-    },
-    button: {
-        fontSize: Font.SMALL,
-        color: Colors.FOREGROUND,
-        borderRadius: 5,
-        fontWeight: 'bold',
-    },
-    title: {
-        fontSize: Font.MEDIUM
-    },
-    label: {
-        fontSize: Font.SMALL,
-        marginBottom: 10,
-
-    },
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: Colors.WHITE,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  buttonContainer: {
+    flexDirection: "row-reverse",
+    alignItems: "flex-start",
+    columnGap: 10,
+  },
+  diaryContainer: {
+    backgroundColor: Colors.WHITE,
+    padding: 15,
+    marginTop: 20,
+    borderRadius: 5,
+    borderWidth: 0.25,
+  },
+  button: {
+    fontSize: Font.SMALL,
+    color: Colors.FOREGROUND,
+    borderRadius: 5,
+    fontWeight: "bold",
+  },
+  title: {
+    fontSize: Font.MEDIUM,
+  },
+  label: {
+    fontSize: Font.SMALL,
+    marginBottom: 10,
+  },
 });
 
 export default DiaryDetail;
